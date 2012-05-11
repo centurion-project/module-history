722415f0856d003698d2ef89b4b44305c8ed93e8
Laurent <lc@centurion-projec.org>
2012-05-11 10:19:59 +0200

    Add infos when seeing an old version

--- traits/Controller/CRUD.php
+++ traits/Controller/CRUD.php
@@ -1,14 +1,18 @@
 <?php
+
 class History_Traits_Controller_CRUD extends Translation_Traits_Controller
 {
     public function restoreAction()
     {
         $logRow = Centurion_Db::getSingleton('history/log')->findOneBy('id', $this->_controller->getRequest()->getParam('restoreId'));
 
-
         $data = unserialize($logRow->value);
 
         $this->_controller->getForm()->populate($data);
+
+        $this->view->infos = array();
+        $this->view->infos[] = $this->view->translate('You are currently seeing an old version of this row. <br />
+                                                If you want to restore it, click on save.<br /> Date: %s', $logRow->getDateObjectBy('created_at')->toString(Zend_Date::DATETIME_MEDIUM), $logRow->name);
 
         $this->getAction();
     }
